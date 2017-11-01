f9c2610292ad1e656af59f2694c1a151cc158bf5
C0ns7ruc7 <00093215@student.rocleiden.nl>
2017-11-01 09:13:23 +0100

    i did something
    i don't rmemeber

--- codeheap/magic.js
+++ codeheap/magic.js
@@ -1,3 +1,7 @@
 function MagicObject(){
-	this.intial-entorpy: Math.floor((Math.random() * 1000000) + 1);
+	var intial_entorpy = Math.floor((Math.random() * 1000000) + 1);
+	this.do = function(){
+		console.log(intial_entorpy);
+	}
 }
+Orb = new MagicObject;
